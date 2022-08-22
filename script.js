2b35c82eaefab8ff2dcd639d73e58932285bb559
Erdem <erdemdoruk3@gmail.com>
2022-08-22 14:07:30 +0300

    Olacak Olacak Olacak o kadar

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 
 
-function getRandom() {
+// function getRandom() {
 
     const numbers = [0,1,2,3,4,5,6,7,8,9]
     const symbols = ["+", "-", "*", "/"]
@@ -21,6 +21,10 @@ function getRandom() {
         
     let number2 = numbers[Math.floor(Math.random()*numbers.length)];
 
+
+    document.getElementById("number1").innerHTML = number1
+document.getElementById("symbols").innerHTML = symbol
+document.getElementById("number2").innerHTML = number2
 
    
 
@@ -43,23 +47,37 @@ function getRandom() {
     if(input.value == (number1 + number2) && symbol == "+")
     {
         console.log("Doğru bildiniz")
-
+        bakalim()
+        // wasd()
         localStorage.setItem("Score",score + 1)
+
+        document.getElementById("number1").innerHTML = number1
+        document.getElementById("symbols").innerHTML = symbol
+        document.getElementById("number2").innerHTML = number2
+
+
+
     }
     else if(input.value == (number1 - number2) && symbol == "-")
     {
         console.log("Doğru bildiniz")
         localStorage.setItem("Score",score + 1)
+        bakalim()
+        // wasd()
     }
     else if(input.value == (number1 * number2) && symbol == "*")
     {
         console.log("Doğru bildiniz")
         localStorage.setItem("Score",score + 1)
+        bakalim()
+        // wasd()
     }
     else if(input.value == (number1 / number2) && symbol == "/")
     {
         console.log("Doğru bildiniz")
         localStorage.setItem("Score",score + 1)
+        bakalim()
+        // wasd()
     }
     else
     {
@@ -73,9 +91,9 @@ function getRandom() {
     console.log(localStorage.getItem("Score"))
 // })
 
-document.getElementById("number1").innerHTML = deneme()
-document.getElementById("symbols").innerHTML = deneme2()
-document.getElementById("number2").innerHTML = deneme()
+document.getElementById("number1").innerHTML = number1
+document.getElementById("symbols").innerHTML = symbol
+document.getElementById("number2").innerHTML = number2
 
 }
 
@@ -88,27 +106,74 @@ document.getElementById("number2").innerHTML = deneme()
 
         //your code
     }
-}
-getRandom();
+
+
+
+
+
+    function bakalim(){
+        document.getElementById("number1").innerHTML = number1
+document.getElementById("symbols").innerHTML = symbol
+document.getElementById("number2").innerHTML = number2
+    }
+// }
+// getRandom();
 
 
 // document.getElementById("input").addEventListener("keypress",setQuery)
 
 
-function deneme()
-{
-    return Math.floor(Math.random() * 10);
-}
+// function deneme()
+// {
+//     return Math.floor(Math.random() * 10);
+// }
 
 
-function deneme2()
-{
-    const symbols = ["+", "-", "*", "/"]
-    return symbols[Math.floor(Math.random()*symbols.length)];
-    console.log(symbols[Math.floor(Math.random()*symbols.length)])
-}
+// function deneme2()
+// {
+//     const symbols = ["+", "-", "*", "/"]
+//     return symbols[Math.floor(Math.random()*symbols.length)];
+//     console.log(symbols[Math.floor(Math.random()*symbols.length)])
+// }
 
 
 
 // document.getElementById("input").addEventListener("keypress",setQuery()) 
    // console.log("31")
+
+
+
+
+
+//    function wasd(){
+//     const numbers = [0,1,2,3,4,5,6,7,8,9]
+//     const symbols = ["+", "-", "*", "/"]
+
+
+//     let number1 = numbers[Math.floor(Math.random()*numbers.length)];
+
+//     let symbol = symbols[Math.floor(Math.random()*symbols.length)];
+        
+//     let number2 = numbers[Math.floor(Math.random()*numbers.length)];
+//    }
+
+
+
+//    class erdem{
+
+//  numbers = [0,1,2,3,4,5,6,7,8,9]
+// symbols = ["+", "-", "*", "/"]
+
+
+//     number1 = numbers[Math.floor(Math.random()*numbers.length)];
+
+//   symbol = symbols[Math.floor(Math.random()*symbols.length)];
+        
+//  number2 = numbers[Math.floor(Math.random()*numbers.length)];
+
+//    }
+
+
+
+
+//    erdem.adim
