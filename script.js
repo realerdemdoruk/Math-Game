8e54dec2b32bbd49477a5cb9bd432cb9135bb6a2
Erdem <erdemdoruk3@gmail.com>
2022-08-25 13:55:50 +0300

    Olmuyor ya

--- script.js
+++ script.js
@@ -2,73 +2,10 @@ const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 const symbols = ["+", "-", "*", "/"]
 
 
-let number1 = numbers[Math.floor(Math.random() * numbers.length)];
-let symbol = symbols[Math.floor(Math.random() * symbols.length)];
-let number2 = numbers[Math.floor(Math.random() * numbers.length)];
+
+let score = document.getElementById("score");
 
 
-// document.getElementById("number1").innerHTML = number1
-// document.getElementById("symbols").innerHTML = symbol
-// document.getElementById("number2").innerHTML = number2
-
-
-
-// document.body.onkeydown = function (e) {
-//     if (e.keyCode == 32) {
-
-//         input = document.getElementById("input")
-
-//         console.log(input.value, "sadas")
-
-
-//         score = 0;
-
-//         console.log(numbers)
-
-//         if (input.value == (number1 + number2) && symbol == "+") {
-//             console.log("Doğru bildiniz")
-//             bakalim()
-//             // wasd()
-//             localStorage.setItem("Score", score + 1)
-
-      
-//             document.getElementById("number1").innerHTML = number1
-//             document.getElementById("symbols").innerHTML = symbol
-//             document.getElementById("number2").innerHTML = number2
-
-
-//         } else if (input.value == (number1 - number2) && symbol == "-") {
-//             console.log("Doğru bildiniz")
-//             localStorage.setItem("Score", score + 1)
-
-//         } else if (input.value == (number1 * number2) && symbol == "*") {
-//             console.log("Doğru bildiniz")
-//             localStorage.setItem("Score", score + 1)
-
-//         } else if (input.value == (number1 / number2) && symbol == "/") {
-//             console.log("Doğru bildiniz")
-//             localStorage.setItem("Score", score + 1)
-
-//         } else {
-//             console.log("Yanlış")
-//         }
-
-//         input.value = ""
-
-
-
-//         console.log(localStorage.getItem("Score"))
-//         // })
-
-//         document.getElementById("number1").innerHTML = number1
-//         document.getElementById("symbols").innerHTML = symbol
-//         document.getElementById("number2").innerHTML = number2
-
-//     }
-
-
-//     //your code
-// }
 
 
 
@@ -78,47 +15,51 @@ let number2 = numbers[Math.floor(Math.random() * numbers.length)];
 document.querySelector("button").addEventListener("click",function(){
 
 
-    let randomX = Math.floor(Math.random() * 10);
-    let randomY = Math.floor(Math.random() * 10);
-
-    document.getElementById("number1").innerHTML = randomX
-    document.getElementById("symbols").innerHTML = symbol
-    document.getElementById("number2").innerHTML = randomY
-
+    let number1 = numbers[Math.floor(Math.random() * numbers.length)];
+    let symbol = symbols[Math.floor(Math.random() * symbols.length)];
+    let number2 = numbers[Math.floor(Math.random() * numbers.length)];
     // console.log("3131")
 
-console.log(randomX, symbol)
 
-// console.log(input.value)
-    if (input.value == (randomX + randomY) && symbol == "+") {
+
+    document.getElementById("number1").innerHTML = number1
+    document.getElementById("symbols").innerHTML = symbol
+    document.getElementById("number2").innerHTML = number2
+    
+    // number31 = Math.floor(Math.random() *9)
+    // console.log(number31)
+
+deneme()    
+
+})
+
+
+
+
+
+
+function deneme(){
+
+    
+    if (input.value == (number1 + number2) && symbol == "+") {
         console.log("Doğru bildiniz 1")
         
-        // bakalim()
-        // wasd()
-        localStorage.setItem("Score", score + 1)
-
-
-
-
-    } else if (input.value == (randomX - randomY) && symbol == "-") {
+    } else if (input.value == (number1 - number2) && symbol == "-") {
         console.log("Doğru bildiniz 2")
-        localStorage.setItem("Score", score + 1)
-        // bakalim()
-        // wasd()
-    } else if (input.value == (randomX * randomY) && symbol == "*") {
+        
+    } else if (input.value == (number1 * number2) && symbol == "*") {
         console.log("Doğru bildiniz 3")
-        localStorage.setItem("Score", score + 1)
-        // bakalim()
-        // wasd()
-    } else if (input.value == (randomX / randomY) && symbol == "/") {
+        
+    } else if (input.value == (number1 / number2) && symbol == "/") {
         console.log("Doğru bildiniz 4")
-        localStorage.setItem("Score", score + 1)
-        // bakalim()
-        // wasd()
+        
     } else {
         console.log("Yanlış")
     }
-
+    
     input.value = ""
+}
 
-})
+// document.getElementById("number1").innerHTML 
+// document.getElementById("symbols").innerHTML 
+// document.getElementById("number2").innerHTML 
