8757d81dbb616a5b9b3d5d58561f59cc2f035f01
Erdem <erdemdoruk3@gmail.com>
2022-08-25 17:06:11 +0300

    Olmadı be abi

--- script.js
+++ script.js
@@ -1,100 +1,50 @@
-const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const symbols = ["+", "-", "*", "/"]
+input = document.getElementById("input");
+
+getRandomSign = () => {
+    const signs = ['+', '-', '*', '/'];
+    const idx = Math.floor(Math.random() * signs.length);
+    return signs[idx];
+};
+
+getRandomNumber = () => {
+    const numbers =[0,1,2,3,4,5,6,7,8,9]
+    const idx = Math.floor(Math.random() * numbers.length);
+    return numbers[idx]
+};
+
+number1 = document.getElementById("number1");
+number2 = document.getElementById("number2");
+symbols = document.getElementById("symbols");
+
+document.querySelector("button").addEventListener("click",function(){
+
+
+    number1.innerHTML = getRandomNumber()
+    symbols.innerHTML = getRandomSign()
+    number2.innerHTML = getRandomNumber()
 
 
 
-let score = document.getElementById("score");
 
-let number1 = numbers[Math.floor(Math.random() * numbers.length)];
-let symbol = symbols[Math.floor(Math.random() * symbols.length)];
-let number2 = numbers[Math.floor(Math.random() * numbers.length)];
+deneme(getRandomNumber(),getRandomNumber(),getRandomSign());
 
-function wasd(){
-
-    document.getElementById("number1").innerHTML = number1
-    document.getElementById("symbols").innerHTML = symbol
-    document.getElementById("number2").innerHTML = number2
-
-}
-
-
-
-document.querySelector("button").addEventListener("click", function(){
-    
-    
-    
-    // console.log("3131")
-    
-
-    
-    // wasd();
-    
-    // document.getElementById("number1").innerHTML = number1
-    // document.getElementById("symbols").innerHTML = symbol
-    // document.getElementById("number2").innerHTML = number2
-    wasd();
-
-
-
-  
-    
-    // number31 = Math.floor(Math.random() *9)
-    // console.log(number31)
-
-deneme(number1,number2,symbol)    
 
 })
 
 
 
 
-
-
-// function deneme(){
-
-    
-//     if (input.value == (number1 + number2) && symbol == "+") {
-//         console.log("Doğru bildiniz 1")
-        
-//     } else if (input.value == (number1 - number2) && symbol == "-") {
-//         console.log("Doğru bildiniz 2")
-        
-//     } else if (input.value == (number1 * number2) && symbol == "*") {
-//         console.log("Doğru bildiniz 3")
-        
-//     } else if (input.value == (number1 / number2) && symbol == "/") {
-//         console.log("Doğru bildiniz 4")
-        
-//     } else {
-//         console.log("Yanlış")
-//     }
-    
-//     input.value = ""
-// }
-
-// document.getElementById("number1").innerHTML 
-// document.getElementById("symbols").innerHTML 
-// document.getElementById("number2").innerHTML 
-
-
-
-
-
-
-
 function deneme(num1, num2, sym){
-
     
     if (input.value == (num1 + num2) && sym == "+") {
         console.log("Doğru bildiniz 1")
         
-        wasd();
     } else if (input.value == (num1 - num2) && sym == "-") {
         console.log("Doğru bildiniz 2")
-        wasd();
+
     } else if (input.value == (num1 * num2) && sym == "*") {
         console.log("Doğru bildiniz 3")
-        wasd()
+
     } else if (input.value == (num1 / num2) && sym == "/") {
         console.log("Doğru bildiniz 4")
         
