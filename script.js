231eefd428bdc97848c4ca92ec8199922b3bcb57
Erdem <erdemdoruk3@gmail.com>
2022-08-30 20:25:01 +0300

    Tasarım hariç olur gibi

--- script.js
+++ script.js
@@ -1,110 +1,79 @@
-let n1 = Math.floor(Math.random()*20+1);
-      let n2 = Math.floor(Math.random()*20+1);
+let n1 = Math.floor(Math.random() * 20 + 1);
+let n2 = Math.floor(Math.random() * 20 + 1);
 
-      document.getElementById("intext").value = n1;
-      document.getElementById("intext1").value = n2;
-      let topScore = document.getElementById("topScore");
+document.getElementById("intext").value = n1;
+document.getElementById("intext1").value = n2;
+let topScore = document.getElementById("topScore");
 
-      let adds= n1 + n2;
+let adds = n1 + n2;
 
-      console.log(adds)
+console.log(adds)
 
-      function Game(){
-         var user = document.getElementById("intext2").value;
+function Game() {
 
+    var user = document.getElementById("intext2").value;
 
-         countdown(30);
+    countdown(30);
 
-         if( user == adds){
-            document.getElementById("ans").innerHTML = "Well Done! Your Answer is Correct";
+    if (user == adds) {
+        document.getElementById("ans").innerHTML = "Well Done! Your Answer is Correct";
 
-            var scorePrint = document.getElementById("score");
+        setLocalStorage();
 
+    } else {
+        document.getElementById("ans").innerHTML = "Correct Answer " + adds + " . Try Again";
+        countdown(1);
+    }
 
-            
-        //     let scoreValue = score.value = Number(score.value) + 1;
-        // scorePrint.innerHTML = `Score: ${scoreValue}`;
+    var user = document.getElementById("intext2").value = "";
 
+    n1 = Math.floor(Math.random() * 20 + 1);
+    n2 = Math.floor(Math.random() * 20 + 1);
 
+    document.getElementById("intext").value = n1;
+    document.getElementById("intext1").value = n2;
 
+    adds = n1 + n2;
 
-        // if (scoreValue > localStorage.getItem("highscore")) {
+}
 
-        //     localStorage.setItem("highscore", scoreValue)
-        //     scorePrint.innerHTML = `Top Score: ${scoreValue}`
-        // }
+// Countdown
+function countdown(e) {
 
+    let timeleft = e;
+    let downloadTimer = setInterval(function () {
+        if (timeleft <= 0) {
+            clearInterval(downloadTimer);
+            document.getElementById("countdown").innerHTML = "Finished";
 
+            if (document.getElementById("countdown").innerHTML == "Finished") {
 
-        let scoreValue = score.value = Number(score.value) + 1
-        scorePrint.innerHTML = `Score: ${scoreValue}`
-
-
-        if (scoreValue > localStorage.getItem("highscore")) {
-
-            localStorage.setItem("highscore", scoreValue)
-            topScore.innerHTML = `Top Score: ${scoreValue}`
+                // Reloads the page
+                setInterval(function () {
+                    window.location.reload(false);
+                }, 2000);
+            }
+        } else {
+            document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
         }
+        timeleft -= 1;
+    }, 1000);
+}
 
-        
-            
+// localStorage
+function setLocalStorage() {
 
-            
+    let scorePrint = document.getElementById("score");
 
+    let scoreValue = score.value = Number(score.value) + 1
+    scorePrint.innerHTML = `Score: ${scoreValue}`
 
-         }else{
-             document.getElementById("ans").innerHTML = "Correct Answer " + adds + " . Try Again";
-             countdown(3);
-         }
+    if (scoreValue > localStorage.getItem("highscore")) {
 
-           var user= document.getElementById("intext2").value = "";
-
-
-      n1 = Math.floor(Math.random()*20+1);
-      n2 = Math.floor(Math.random()*20+1);
-
-      document.getElementById("intext").value = n1;	
-      document.getElementById("intext1").value = n2;
-
-      adds = n1 + n2;
+        localStorage.setItem("highscore", scoreValue)
+        topScore.innerHTML = `Top Score: ${scoreValue}`
+    }
+}
 
 
-      }
-
-
-
-
-
-
-
-    // Countdown
-      function countdown(e){
-
-          let timeleft = e;
-          let downloadTimer = setInterval(function () {
-              if (timeleft <= 0) {
-                  clearInterval(downloadTimer);
-                  document.getElementById("countdown").innerHTML = "Finished";
-                  
-                  if (document.getElementById("countdown").innerHTML == "Finished") {
-                      //   random.style.display = "none";
-                      
-                      // Reloads the page
-                      setInterval(function () {
-                          window.location.reload(false);
-                        }, 2000);
-                    }
-                } 
-                else {
-                    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-                }
-                timeleft -= 1;
-            }, 1000);
-            
-
-        }
-
-
-
-      
-        topScore.innerHTML = `Top Score: ${localStorage.getItem("highscore")}`
+topScore.innerHTML = `Top Score: ${localStorage.getItem("highscore")}`
